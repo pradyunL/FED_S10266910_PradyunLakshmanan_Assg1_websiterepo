f2115defe9bddfc9e2c5d6a71ffde7c05d42c160
pradyunL <s10266910@connect.np.edu.sg>
2024-11-30 23:47:44 +0800

    Hamburger Menu Icon + Images added into Scroll by Sports Homepage

--- docs/script.js
+++ docs/script.js
@@ -22,4 +22,29 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3200);
 }
+
+//Hamburger Menu
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const hamburgerDropdown = document.querySelector(".hamburger-dropdown");
+
+  hamburgerIcon.addEventListener("click", () => {
+    const isMenuVisible = hamburgerDropdown.classList.contains("active");
+    if (isMenuVisible) {
+      hamburgerDropdown.classList.remove("active");
+    } else {
+      hamburgerDropdown.classList.add("active");
+    }
+  });
+
+  document.addEventListener("click", (event) => {
+    if (
+      !hamburgerDropdown.contains(event.target) &&
+      event.target !== hamburgerIcon
+    ) {
+      hamburgerDropdown.classList.remove("active");
+    }
+  });
+});
   
